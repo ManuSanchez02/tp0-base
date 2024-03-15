b132013ae44656fe2c2b4407ea422c3a97fc4ace
Manu <manu.sfdlv@gmail.com>
2024-03-14 21:04:23 -0300

    ejercicio 1

---
+++ ejercicio_1/main.py
@@ -0,0 +1,75 @@
+from argparse import ArgumentParser
+import yaml
+
+def create_config(clients):
+    config = {
+        "version": "1.0",
+        "name": "ejercicio_1_1",
+        "services": {
+                "server": {
+                    "container_name": "server",
+                    "image": "server:latest",
+                    "entrypoint": "python3 /main.py",
+                    "environment": [
+                        "PYTHONUNBUFFERED=1",
+                        "LOGGING_LEVEL=DEBUG"
+                    ],
+                    "networks": [
+                        "testing_net"
+                    ]
+                }
+        },
+        "networks": {
+            "testing_net": {
+                "ipam": {
+                    "driver": "default",
+                    "config": [
+                        {
+                            "subnet": "172.25.125.0/24"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+
+    for i in range(clients):
+        client_id = i+1
+        client_config = {
+            "container_name": f"client{client_id}",
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [
+                f"CLI_ID={client_id}",
+                "CLI_LOG_LEVEL=DEBUG"
+            ],
+            "networks": [
+                "testing_net"
+            ],
+            "depends_on": [
+                "server"
+            ]
+        }
+        config["services"][f"client{client_id}"] = client_config
+
+    return config
+
+
+def get_client_number():
+    parser = ArgumentParser(
+                    prog='Docker compose config generator',
+                    description='Generate n clients')
+    parser.add_argument('client_number', type=int, help="Number of clients")
+    args = parser.parse_args()
+    return args.client_number
+
+
+def main():
+    client_number = get_client_number()
+    file_data = create_config(client_number)
+    with open("docker-compose-ej-1.yaml", "w") as file:
+        yaml.dump(file_data, file, default_flow_style=False, sort_keys=False)
+
+
+if __name__ == "__main__":
+    main()
